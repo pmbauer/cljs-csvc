cf53cffdb6092a2223c2bc56ff7a8015cef711c1
Paul M Bauer <paul.michael.bauer@gmail.com>
2011-11-14 20:27:31 -0700

    version added

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.deftitlenil/cljs-csvc "1.0.0-SNAPSHOT"
+(defproject com.deftitlenil/cljs-csvc "0.1.0-SNAPSHOT"
   :description "Clojure analytics workshop example"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
